1513fce93ecef70354696d7cd4a17a24a6d2ecf3
mansigeek <mansimgeek435@gmail.com>
2025-12-26 14:49:55 +0530

    publish widget

--- dist/auto-connect-widget.js
+++ dist/auto-connect-widget.js
@@ -25,166 +25,110 @@
     font-size:14px;line-height:1.6;opacity:0.9;
 }
 
-.ac-49d532b2 {
-    background-color:#5d74a2;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-06879c89 {
+    background-color:#f3f4f6;border:none;border-radius:1px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-86c1700a {
-    background-color:#5f76a5;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-215393d5 {
+    background-color:#f3f4f6;border:none;border-radius:10px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-632446e9 {
-    background-color:#5f78ab;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-f8d06d40 {
+    background-color:#f3f4f6;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-c5acf21c {
-    background-color:#607aaf;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-4cd212d5 {
+    background-color:#194294;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-f0facb8f {
-    background-color:#5f7cb4;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-1aec9722 {
+    background-color:#1a4499;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-2cc916a7 {
-    background-color:#5b7ab9;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-c59ddc80 {
+    background-color:#19449a;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-babe8fc2 {
-    background-color:#5a7dc4;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-3744f13b {
+    background-color:#19459f;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-b76fa786 {
-    background-color:#5178c8;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-80e81782 {
+    background-color:#1748ab;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-122d5b4a {
-    background-color:#4b76ce;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-28be8e31 {
+    background-color:#164ab1;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-cc12d085 {
-    background-color:#4171d2;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-72364bdc {
+    background-color:#134ec3;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-810cdf95 {
-    background-color:#366edd;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-f44ff761 {
+    background-color:#124fca;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-955b548a {
-    background-color:#2c69e2;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-941d6ee0 {
+    background-color:#0e51d8;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-13be7948 {
-    background-color:#1c5ee3;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-68ee7975 {
+    background-color:#0c52df;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-9980b3a6 {
-    background-color:#1258e2;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-ea31a4e8 {
+    background-color:#0654ef;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-fbb73a6d {
-    background-color:#0451ec;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-650d0c60 {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-f9dd7bd5 {
-    background-color:#0050f0;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-9b3e1688 {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#637aab;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-17b4a5a4 {
-    background-color:#0052f5;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-14d93f3b {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#687dac;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-8798ccb6 {
-    background-color:#0053fa;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-a74d0430 {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#7a8eb8;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-fbabddef {
-    background-color:#0055ff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-b22f4da7 {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#8697bb;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-303b604d {
-    background-color:#001eff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-79a3256d {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#99a8c7;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-4e5945da {
-    background-color:#002aff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-a1a2bd10 {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#adb8d2;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-5e550093 {
-    background-color:#0033ff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-452c441f {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#c8cfdf;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-53fa3f7a {
-    background-color:#004cff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-70e1fdb2 {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#dde1e9;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-5682cd23 {
-    background-color:#0062ff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-8d7426ef {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#fcfcfc;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-7f2d9360 {
-    background-color:#007bff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-039b6ed5 {
-    background-color:#009dff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-13ee295b {
-    background-color:#00bfff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-aad36b45 {
-    background-color:#00d5ff;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-f32362f5 {
-    background-color:#00ffee;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-4aa271b9 {
-    background-color:#00ffbf;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-c71807fb {
-    background-color:#00ff9d;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-66e8f58f {
-    background-color:#00ff6e;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-c257f9e2 {
-    background-color:#00ff2a;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-993ee96b {
-    background-color:#00ff11;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-46c950d9 {
-    background-color:#00ff04;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-4b1c28a1 {
-    background-color:#04ff00;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-0386026a {
-    background-color:#04ff00;border:none;border-radius:1px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-ed7d185d {
-    background-color:#04ff00;border:none;border-radius:10px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-c25ef209 {
-    background-color:#04ff00;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-b76f6e3e {
+    background-color:#0055ff;border:none;border-radius:100px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#ffffff;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 `;
 
   // Inject HTML
   const wrapper = document.createElement("div");
-  wrapper.innerHTML = `<div id="auto-connect-preview-root"><div class="ac-c25ef209"><h4 class="ac-3ea1708e">Card Title</h4><p class="ac-6b5c8d0e">This is a preview card. Styles applied here will be exported exactly the same into WordPress. All styles are dynamically generated CSS classes and are completely WordPress-safe.</p></div></div>`;
+  wrapper.innerHTML = `<div id="auto-connect-preview-root"><div class="ac-b76f6e3e"><h4 class="ac-3ea1708e">Card Title</h4><p class="ac-6b5c8d0e">This is a preview card. Styles applied here will be exported exactly the same into WordPress. All styles are dynamically generated CSS classes and are completely WordPress-safe.</p></div></div>`;
 
   shadow.appendChild(style);
   shadow.appendChild(wrapper);
