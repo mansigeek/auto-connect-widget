bdee05c681aa71b018ddd1a40e4cc86bf662d99c
mansigeek <mansimgeek435@gmail.com>
2025-12-26 15:12:33 +0530

    publish widget

--- dist/auto-connect-widget.js
+++ dist/auto-connect-widget.js
@@ -25,86 +25,30 @@
     font-size:14px;line-height:1.6;opacity:0.9;
 }
 
-.ac-4d141b91 {
-    background-color:#214387;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-a0153cf8 {
+    background-color:#f3f4f6;border:none;border-radius:4px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-881c1fe2 {
-    background-color:#22458c;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-47270c9d {
+    background-color:#f3f4f6;border:none;border-radius:40px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-2db120ab {
-    background-color:#1f448e;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-8a917e0f {
+    background-color:#f3f4f6;border:none;border-radius:2px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-d3493cbf {
-    background-color:#1e448f;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
+.ac-4ca0006f {
+    background-color:#f3f4f6;border:none;border-radius:20px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 
-.ac-5c24dccd {
-    background-color:#1f4693;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-45323906 {
-    background-color:#1e4694;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-a6776dcf {
-    background-color:#1c469c;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-b96301ef {
-    background-color:#19459f;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-e6b98077 {
-    background-color:#1748ab;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-d24327d8 {
-    background-color:#1447ad;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-77da4e94 {
-    background-color:#1148b6;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-92f9efa0 {
-    background-color:#0d47ba;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-033c2d76 {
-    background-color:#0949c8;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-f44f8a94 {
-    background-color:#0447cd;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-596cffcd {
-    background-color:#0046d1;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-5c33b42f {
-    background-color:#004be0;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
-}
-
-.ac-fb02fa03 {
-    background-color:#004be0;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:1px;transition:all 150ms ease;
-}
-
-.ac-482acea6 {
-    background-color:#004be0;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:10px;transition:all 150ms ease;
-}
-
-.ac-23cd5084 {
-    background-color:#004be0;border:none;border-radius:12px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:100px;transition:all 150ms ease;
+.ac-b2ac353e {
+    background-color:#6f95e2;border:none;border-radius:20px;box-shadow:0 1px 2px rgba(0,0,0,0.05);color:#111827;font-family:inherit;max-width:28rem;padding:16px;transition:all 150ms ease;
 }
 `;
 
   // Inject HTML
   const wrapper = document.createElement("div");
-  wrapper.innerHTML = `<div id="auto-connect-preview-root"><div class="ac-23cd5084"><h4 class="ac-3ea1708e">Card Title</h4><p class="ac-6b5c8d0e">This is a preview card. Styles applied here will be exported exactly the same into WordPress. All styles are dynamically generated CSS classes and are completely WordPress-safe.</p></div></div>`;
+  wrapper.innerHTML = `<div id="auto-connect-preview-root"><div class="ac-b2ac353e"><h4 class="ac-3ea1708e">Card Title</h4><p class="ac-6b5c8d0e">This is a preview card. Styles applied here will be exported exactly the same into WordPress. All styles are dynamically generated CSS classes and are completely WordPress-safe.</p></div></div>`;
 
   shadow.appendChild(style);
   shadow.appendChild(wrapper);
